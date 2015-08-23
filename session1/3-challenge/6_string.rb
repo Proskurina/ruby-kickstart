a2578d17f492ceb1f63a14fb110de1970eaa61dd
Proskurina <yproskurina@yahoo.com>
2015-08-24 00:18:43 +0100

    Add session 1 exercises

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -5,6 +5,27 @@
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
 #
 
-def odds_and_evens(string, return_odds)
 
+#Some ugly code here. When I saw the original solution my jaw dropped)))
+
+def odds_and_evens(string, return_odds)
+	odd = []
+	even = []
+	ar = string.split('')
+	ar.each do |char|
+		if ar.index(char).even?
+			even.push(char)
+		else
+			odd.push(char)
+		end
+	end
+	odd = odd.join
+	even = even.join
+	if return_odds
+		return odd
+	else
+		return even
+	end
 end
+
+puts odds_and_evens('abcdefg', false)
--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -4,6 +4,19 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
+#Here the original solution also was very surprising!!! Love it!
 
+def pirates_say_arrrrrrrrr(string)
+	result = ''
+	string.length.times do |index|
+		if string[index] == 'r' || string[index] == 'R'
+			result << string[index+1].to_s
+		end
+	end
+	return result
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
+
